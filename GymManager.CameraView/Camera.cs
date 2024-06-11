cc7c17c981f14c40265e04069f659e110cd74655
Kamil Wojcieszek <kwojcieszek@radwag.pl>
2024-06-11 08:43:15 +0200

    Refactoring.

---
+++ GymManager.CameraView/Camera.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
+using Windows.Media.MediaProperties;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Streams;
+using Windows.Storage;
+using System.IO;
+using Windows.Media.Capture;
+
+namespace CameraView
+{
+    internal class Camera
+    {
+        public async Task<bool> CameraCaptutre(MediaCapture capture)
+        {
+            try
+            {
+                var pictures = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+                var file = await pictures.SaveFolder.CreateFileAsync("CameraCaptutre.jpg", CreationCollisionOption.ReplaceExisting);
+
+                using (var captureStream = new InMemoryRandomAccessStream())
+                {
+                    await capture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), captureStream);
+
+                    using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                    {
+                        var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(captureStream);
+                        var encoder = await Windows.Graphics.Imaging.BitmapEncoder.CreateForTranscodingAsync(fileStream, decoder);
+
+                        var properties = new BitmapPropertySet
+                        {
+                            {
+                                "System.Photo.Orientation", new BitmapTypedValue(PhotoOrientation.Normal, PropertyType.UInt16)
+                            }
+                        };
+                        await encoder.BitmapProperties.SetPropertiesAsync(properties);
+
+                        await encoder.FlushAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                File.Delete("CameraCaptutre.jpg");
+
+                throw new Exception("Error while capturing image", ex);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ GymManager.CameraView/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Windows;
+using Windows.Media.Capture;
+using MediaCaptureWPF;
+
+namespace CameraView
+{
+    public partial class MainWindow : MetroWindow
+    {
+        private bool _initialized;
+        private MediaCapture _capture;
+        private readonly Camera _camera = new Camera();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnActivated(EventArgs e)
+        {
+            if (_initialized)
+                return;
+
+            _initialized = true;
+
+            try
+            {
+                _capture = new MediaCapture();
+
+                await _capture.InitializeAsync(new MediaCaptureInitializationSettings
+                {
+                    StreamingCaptureMode = StreamingCaptureMode.Video //No audio
+                });
+
+                var preview = new CapturePreview(_capture);
+
+                this.Preview.Source = preview;
+
+                await preview.StartAsync();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(await _camera.CameraCaptutre(_capture))
+                {
+                    this.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
